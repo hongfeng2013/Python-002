6b08720ab056f29df0113a9d71ed556c3aa8254f
hongfeng <hongfengzh@hotmail.com>
2020-09-13 22:24:40 +0800

    add @wraps

--- week08/homework3.py
+++ week08/homework3.py
@@ -1,9 +1,11 @@
 # 实现一个 @timer 装饰器，记录函数的运行时间，注意需要考虑函数可能会接收不定长参数。
 
 import time
+from functools import wraps
 
 
 def timer(func):
+    @wraps(func)
     def func_wrapper(*args, **kwargs):
         print("####################In func_wrapper!")
         start = time.time()
